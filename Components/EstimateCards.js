d7237b2b9008e28eb8f89f724fe459fc682686c6
Garvit <dudejagarvit3@gmail.com>
2025-07-10 21:30:50 +0530

    updated colors

--- Components/EstimateCards.js
+++ Components/EstimateCards.js
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { MdOutlineMail } from "react-icons/md";
+import { FaDollarSign, FaRegLightbulb  } from "react-icons/fa";
+import { SlGraph } from "react-icons/sl";
+
+
 
 const EstimateSent = ({ data, next }) => {
   return (
     <Container className="d-flex justify-content-center align-items-center">
       <Card className="p-4 shadow" style={{ maxWidth: '500px', width: '100%' }}>
-        <h3 className="text-primary mb-3">
-          📩 Estimate Sent!
+        <h3 className="mb-3 blueText">
+          <MdOutlineMail  size={34} className='mb-1'/> Estimate Sent!
         </h3>
         <p>We’ve sent your custom life insurance estimate to:</p>
         <h5 className="fw-bold">swolkov@me.com</h5>
@@ -15,9 +20,9 @@ const EstimateSent = ({ data, next }) => {
         <div className="mt-4">
           <h6 className="fw-bold">What’s Inside:</h6>
           <ul className="list-unstyled ms-2">
-            <li>💲 Your monthly contribution</li>
-            <li>📈 Projected cash value</li>
-            <li>💡 Overview of your potential policy</li>
+            <li><FaDollarSign className='blueText'/> Your monthly contribution</li>
+            <li><SlGraph className='blueText'/> Projected cash value</li>
+            <li><FaRegLightbulb className='blueText'/> Overview of your potential policy</li>
           </ul>
         </div>
 
@@ -31,7 +36,7 @@ const EstimateSent = ({ data, next }) => {
         <p className="text-muted mt-3" style={{ fontSize: '0.9rem' }}>
           If you don’t see it in your inbox within a few minutes, check your spam or promotions folder.
         </p>
-        <Link href="https://globeintegrity.com/" className="text-decoration-none mt-2 d-block">Back to Homepage</Link>
+        <Link href="https://globeintegrity.com/" className="text-decoration-none blueText mt-2 d-block">Back to Homepage</Link>
       </Card>
     </Container>
   );
